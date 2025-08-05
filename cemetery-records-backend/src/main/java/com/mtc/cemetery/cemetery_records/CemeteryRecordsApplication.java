6f29c2eaae6cb5dd14a708afbe1dd77f10368415
Matthias <tioarrindell@gmail.com>
2025-08-05 00:46:47 -0400

    initial commit

---
+++ cemetery-records-backend/src/main/java/com/mtc/cemetery/cemetery_records/CemeteryRecordsApplication.java
@@ -0,0 +1,13 @@
+package com.mtc.cemetery.cemetery_records;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CemeteryRecordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CemeteryRecordsApplication.class, args);
+	}
+
+}
---
+++ cemetery-records-backend/src/test/java/com/mtc/cemetery/cemetery_records/CemeteryRecordsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtc.cemetery.cemetery_records;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CemeteryRecordsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
